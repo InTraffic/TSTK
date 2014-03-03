7016ccebac15d6938d0b9ef1cbf7e37076265d61
Frank <frank.walinga@intraffic.nl>
2014-03-03 11:42:59 +0100

    Files to make it possible to create a package out of this

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Toolkit_packages',
+    version='0.1.0dev',
+    packages=['cc','focon','train', 'scenarioplayer', 'testcase',],
+    license= open('LICENCE.txt').read(),
+    long_description=open('README.txt').read(),
+)
