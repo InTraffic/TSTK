766a1ca598937117cf83517d2521769f2de1d157
Frank <frank.walinga@intraffic.nl>
2014-03-26 10:29:51 +0100

    simulatorinterface finalization

--- testsystem/simulatorinterface.py
+++ testsystem/simulatorinterface.py
@@ -3,13 +3,11 @@ import zmq
 import copy
 
 class SimulatorInterface(object):
-    """ Simulator interface for communication with the corresponding
-    simulator.
+    """ A generic simulator interface for communication with the 
+    corresponding simulator.
     """
     
     def __init__(self, name, sim_id, zmq_context):
-        """
-        """
         self.logger = logging.getLogger('simulatorinterface.{0}{1}'
                                         .format(name, str(sim_id)))
         self.message_link = zmq_context.socket(zmq.SUB)
@@ -24,13 +22,44 @@ class SimulatorInterface(object):
         self.callbacks = {}
 
     def on_message(self, scenario_player):
-        pass
+        """ Receive a message from the simulator
+
+        :param scenario_player: unused
+        """
+        message = self.message_link.recv_pyobj()
+        self.do_callbacks(message)
     
     def do_callbacks(self, message):
-        pass
+        """ Call the callbacks for the message
+        
+        :param message: the message from the simulator to do the 
+                        callbacks for
+        """
+        if message.message_id in self.callbacks:
+            # Have to operate on a copy because the callback functions
+            # can modify the callback list.
+            for function in copy.copy(self.callbacks[message.message_id]):
+                function(message)
     
     def set_callback(self, message_id, function):
-        pass
+        """ Add a callback function for a message
+
+        :param message_id: the message id
+        :param function: the function to add
+        """
+        if message_id in self.callbacks:
+            self.callbacks[message_id].append(function)
+        else:
+            self.callbacks[message_id] = [function]
     
     def send(self, message):
-        pass
+        """ Send a message to the simulator
+
+        :param message: The message to send to the simulator
+        """
+        self.command_link.send_pyobj(message)
+
+
+
+
+
